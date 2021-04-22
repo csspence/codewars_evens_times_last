6100cc2de3f420f8038582aaac73111f94dc8d4c
Crew Spence <csspence123@gmail.com>
2021-04-22 06:54:53 -0500

    Wrote working solution.

--- codewars_evens_times_last.js
+++ codewars_evens_times_last.js
@@ -4,6 +4,16 @@ Given a sequence of integers, return the sum of all the integers that have an ev
 If the sequence is empty, you should return 0.
 */
 
-function evenLast(numbers) {
-  // Good luck
+const evenLast = (numbers) => {
+  let sum = 0;
+  if(numbers.length === 0) {
+    return 0;
+  }
+  for(let i = 0; i < numbers.length; i++) {
+    if(i % 2 === 0) {
+      sum += numbers[i];
+    }
+  }
+
+  return sum * numbers[numbers.length-1];
 }
